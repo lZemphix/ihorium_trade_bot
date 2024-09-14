308d5e6b975baf6682e450558c92918c7d5cfc10
Zemphix <vafinaaa51@gmail.com>
2024-09-14 17:52:06 +0500

    added 'except ReadTimeout'

--- activate.py
+++ activate.py
@@ -1,19 +1,25 @@
 from datetime import datetime
 from bot import bot, config
 import time, traceback
+from requests.exceptions import ReadTimeout
 
 
 if __name__ == '__main__':
     try:
         bot.start()
+
+    except ReadTimeout as rt:
+        print('err', traceback.format_exc())
+        if config.get('send_notify'):
+            bot.notify.error(f'{rt} at {datetime.now()}')
     except Exception as e:
         print('err', traceback.format_exc())
         if config.get('send_notify'):
-            bot.notify.error_notify(f'Error ({e}) at {datetime.now()}')
+            bot.notify.error(f'{e} at {datetime.now()}')
     finally:
+        print('bot was stopped!')
         if config.get('send_notify'):
-            bot.notify.bot_status(f'starting reboot at {datetime.now()}')
-        print('bot was stopped')
+            bot.notify.error('Bot was stopped!')
         
    
         
